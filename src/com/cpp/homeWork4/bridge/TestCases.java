a4615991947838cf2c2bbff71e288d08e8d8a864
talhaahmed884 <talhaahmed884@gmail.com>
2025-10-14 14:18:48 -0700

    - Added cases for display messages of the classes

--- src/com/cpp/homeWork4/bridge/TestCases.java
+++ src/com/cpp/homeWork4/bridge/TestCases.java
@@ -3,6 +3,9 @@ package com.cpp.homeWork4.bridge;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class TestCases {
     @Test
     public void CashPaymentTest() {
@@ -13,12 +16,32 @@ public class TestCases {
         Assertions.assertNotEquals(SMSNotificationChannel.class, paymentChannel.getNotificationChannel());
         Assertions.assertInstanceOf(CashPaymentChannel.class, paymentChannel);
 
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        PrintStream oldStream = System.out;
+        paymentChannel.makePayment();
+        String correctPaymentMessage = String.format(ChannelMessages.PAYMENT_CHANNEL_MESSAGE + "\n", PaymentChannelType.CASH_PAYMENT_CHANNEL);
+        String correctNotificationMessage = String.format(ChannelMessages.NOTIFICATION_CHANNEL_MESSAGE + "\n\n", NotificationChannelType.EMAIL_CHANNEL);
+        Assertions.assertEquals(correctPaymentMessage + correctNotificationMessage, outputStream.toString());
+        System.setOut(oldStream);
+        outputStream.reset();
+
         paymentChannel = new CashPaymentChannel(new SMSNotificationChannel());
         Assertions.assertEquals(PaymentChannelType.CASH_PAYMENT_CHANNEL, paymentChannel.getPaymentChannelType());
         Assertions.assertNotEquals(PaymentChannelType.ONLINE_PAYMENT_CHANNEL, paymentChannel.getPaymentChannelType());
         Assertions.assertInstanceOf(SMSNotificationChannel.class, paymentChannel.getNotificationChannel());
         Assertions.assertNotEquals(EmailNotificationChannel.class, paymentChannel.getNotificationChannel());
         Assertions.assertInstanceOf(CashPaymentChannel.class, paymentChannel);
+
+        System.setOut(printStream);
+        paymentChannel.makePayment();
+        correctNotificationMessage = String.format(ChannelMessages.NOTIFICATION_CHANNEL_MESSAGE + "\n\n", NotificationChannelType.SMS_CHANNEL);
+        Assertions.assertEquals(correctPaymentMessage + correctNotificationMessage, outputStream.toString());
+        System.setOut(oldStream);
+
+        oldStream.close();
+        printStream.close();
     }
 
     @Test
@@ -30,11 +53,31 @@ public class TestCases {
         Assertions.assertNotEquals(SMSNotificationChannel.class, paymentChannel.getNotificationChannel());
         Assertions.assertInstanceOf(OnlinePaymentChannel.class, paymentChannel);
 
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        PrintStream oldStream = System.out;
+        paymentChannel.makePayment();
+        String correctPaymentMessage = String.format(ChannelMessages.PAYMENT_CHANNEL_MESSAGE + "\n", PaymentChannelType.ONLINE_PAYMENT_CHANNEL);
+        String correctNotificationMessage = String.format(ChannelMessages.NOTIFICATION_CHANNEL_MESSAGE + "\n\n", NotificationChannelType.EMAIL_CHANNEL);
+        Assertions.assertEquals(correctPaymentMessage + correctNotificationMessage, outputStream.toString());
+        System.setOut(oldStream);
+        outputStream.reset();
+
         paymentChannel = new OnlinePaymentChannel(new SMSNotificationChannel());
         Assertions.assertEquals(PaymentChannelType.ONLINE_PAYMENT_CHANNEL, paymentChannel.getPaymentChannelType());
         Assertions.assertNotEquals(PaymentChannelType.CASH_PAYMENT_CHANNEL, paymentChannel.getPaymentChannelType());
         Assertions.assertInstanceOf(SMSNotificationChannel.class, paymentChannel.getNotificationChannel());
         Assertions.assertNotEquals(EmailNotificationChannel.class, paymentChannel.getNotificationChannel());
         Assertions.assertInstanceOf(OnlinePaymentChannel.class, paymentChannel);
+
+        System.setOut(printStream);
+        paymentChannel.makePayment();
+        correctNotificationMessage = String.format(ChannelMessages.NOTIFICATION_CHANNEL_MESSAGE + "\n\n", NotificationChannelType.SMS_CHANNEL);
+        Assertions.assertEquals(correctPaymentMessage + correctNotificationMessage, outputStream.toString());
+        System.setOut(oldStream);
+
+        oldStream.close();
+        printStream.close();
     }
 }
